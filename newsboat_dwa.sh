feaccc9a72dee1ca9c4d8a02d5739ded0dda1f04
dabbott-shq <57420427+dabbott-shq@users.noreply.github.com>
2020-07-24 21:42:08 -0700

    Update newsboat_dwa.sh

--- newsboat_dwa.sh
+++ newsboat_dwa.sh
@@ -3,4 +3,5 @@
 channelid=`w3m -dump_source $1 | gunzip -f | grep "itemprop=\"channelId\"" | sed 's/.*"\(.*\)"[^"]*$/\1/'`
 echo $channelid
 
-echo "https://www.youtube.com/feeds/videos.xml?channel_id=${channelid}" 
+rssurl="https://www.youtube.com/feeds/videos.xml?channel_id="
+echo "${rssurl}${channelid}" 
