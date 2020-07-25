3e10661cf673f3f49c84aa931468c4dd77f6a97b
dabbott-shq <57420427+dabbott-shq@users.noreply.github.com>
2020-07-24 21:53:36 -0700

    Update newsboat_dwa.sh

--- newsboat_dwa.sh
+++ newsboat_dwa.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 #A script to return youtube channel id in stdout
-channelid=`w3m -dump_source $1 | gunzip -f | grep "itemprop=\"channelId\"" | sed 's/.*"\(.*\)"[^"]*$/\1/'`
-echo $channelid
+#channelid=`w3m -dump_source $1 | gunzip -f | grep "itemprop=\"channelId\"" | sed 's/.*"\(.*\)"[^"]*$/\1/'`
+#echo $channelid
 
-rssurl="https://www.youtube.com/feeds/videos.xml?channel_id="
-echo "${rssurl}${channelid}" 
+#rssurl="https://www.youtube.com/feeds/videos.xml?channel_id="
+#echo "${rssurl}${channelid}" 
+
+echo "https://www.youtube.com/feeds/videos.xml?channel_id="`w3m -dump_source $1 | gunzip -f | grep "itemprop=\"channelId\"" | sed 's/.*"\(.*\)"[^"]*$/\1/'`""
